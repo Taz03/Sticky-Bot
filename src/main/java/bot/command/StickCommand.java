77525e1b90d3132667b699a93f86cb04f229d953
Tanish <tanishazad03@gmail.com>
2022-07-11 20:57:34 +0530

    fixed no message id bug

--- src/main/java/bot/command/StickCommand.java
+++ src/main/java/bot/command/StickCommand.java
@@ -12,17 +12,15 @@ public class StickCommand implements MessageCommand {
         String msg = event.getTarget().getContentRaw();
         MessageChannel channel = event.getTarget().getChannel();
 
-        final Long[] messageId = new Long[1];
-
-        channel.sendMessage(msg).queue(message -> messageId[0] = message.getIdLong());
-
-        StickyMessage stickyMessage = new StickyMessage(channel.getIdLong(), msg, messageId[0]);
-
         event.reply("""
                 Sticking Message...
                 Done :thumbsup:""").setEphemeral(true).queue();
 
-        StickyMessageUtils.addSticky(event.getGuild().getIdLong(), stickyMessage);
+        channel.sendMessage(msg).queue(message -> {
+            StickyMessage stickyMessage = new StickyMessage(channel.getIdLong(), msg, message.getIdLong());
+
+            StickyMessageUtils.addSticky(event.getGuild().getIdLong(), stickyMessage);
+        });
     }
 
     @Override
